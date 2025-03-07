bf673e2a1b5be127610376d555bde944dfd87ae3
KrishsNdarcy <krishivb380@stemcollegiate.ca>
2025-03-07 09:50:22 -0700

    Create mar5.cs

---
+++ mar5.cs
@@ -0,0 +1,10 @@
+//March 5
+// Today I learned 
+Console.WriteLine('b');
+Console.WriteLine("Hello world");
+Console.WriteLine(123);
+Console.WriteLine(0.25F);
+Console.WriteLine(2.625);
+Console.WriteLine(12.39816m);
+Console.WriteLine(true);
+Console.WriteLine(false);
