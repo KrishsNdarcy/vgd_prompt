06a182c96fcf102ff430ac6bce2458cb95d63d32
KrishsNdarcy <krishivb380@stemcollegiate.ca>
2025-03-12 08:36:01 -0600

    Create Mar12.cs

---
+++ Mar12.cs
@@ -0,0 +1,10 @@
+//Mar 12
+//Today I learned string interpolation
+string firstName = "Bob";
+string greeting = "Hello";
+Console.WriteLine(greeting + " " + firstName + "!");
+int version = 11;
+string updateText = "Update to Windows";
+Console.WriteLine($"{updateText} {version}!");
+string projectName = "First-Project";
+Console.WriteLine($@"C:\Output\{projectName}\Data");
