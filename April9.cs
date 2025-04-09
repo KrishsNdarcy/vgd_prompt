d468a5820ba983e7c6be324a456f451aeae53866
KrishsNdarcy <krishivb380@stemcollegiate.ca>
2025-04-09 08:35:23 -0600

    Create April9.cs

---
+++ April9.cs
@@ -0,0 +1,13 @@
+// April 9
+int sum = 7 + 5;
+int difference = 7 - 5;
+int product = 7 * 5;
+int quotient = 7 / 5;
+
+Console.WriteLine("Sum: " + sum);
+Console.WriteLine("Difference: " + difference);
+Console.WriteLine("Product: " + product);
+Console.WriteLine("Quotient: " + quotient);
+decimal decimalQuotient = 7.0m / 5;
+Console.WriteLine($"Decimal quotient: {decimalQuotient}");
+//today I learned how to make mathimatical operations in C#
